26c1443c00076f523e4d92b25d599b6754db09ca
Claudia Villarroel <claudinhavilla@hotmail.com>
2020-03-04 23:10:03 -0300

    ADD Clase condicionales a Buenas practicas

---
+++ 06_Condicionales.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+""" Condicionales 
+- Operaciones relacionales (igual mayor)
+- Operaciones logicos (and or not)
+"""
+def say_hello(age):
+    if age > 18:
+        print('Hola senor')
+    else:
+        print('Hola nino')
+
+
+
+
+say_hello(20)
+
