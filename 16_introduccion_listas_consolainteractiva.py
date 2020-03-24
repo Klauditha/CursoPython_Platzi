d64e0deab34c4a4779ef09d704403189baeb38f8
Claudia Villarroel <claudia.villarroel@live.com>
2020-03-24 17:28:46 -0300

    ADD: Introducción a las listas

---
+++ 16_introduccion_listas_consolainteractiva.py
@@ -0,0 +1,10 @@
+### Consola interactiva
+amigos = [] ##Forma 1
+amigos = list() ##Forma 2
+
+amigos.append('pedro')
+amigos.append('enrique')
+amigos.append('alberto')
+
+amigos[0] // pedro
+amigos[2]  //alberto
